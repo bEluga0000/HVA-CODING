4d0fa8e5079c3255fac822c217e499e09efd90e2
shreyas20003 <shreyasas2975@gmail.com>
2024-09-11 22:49:10 +0530

    28 gcd approach

--- prob28.js
+++ prob28.js
@@ -20,3 +20,19 @@ const LCM = (a,b)=>{
 }
 
 LCM(2,3)
+// tc o(n)
+
+// GCD approach
+const HCF = (n1,n2)=>{
+    if(n2 == 0)
+        return n1
+    else
+        return HCF(n2,n1%n2)
+}
+const LCMB = (n1,n2)=>{
+    const hcfVal = HCF(n1,n2)
+    const ans = (n1*n2)/hcfVal
+    console.log(`LCM of ${n1} and ${n2} is ${ans}`)
+    return
+}
+LCMB(20,30)
