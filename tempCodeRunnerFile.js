1c21f0b831df84178c09117e53b85eab915362c7
shreyas20003 <shreyasas2975@gmail.com>
2024-09-12 11:14:05 +0530

    delete

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-
-    let i = count-1
-    while(i >= 0)
-    {
-        let n = parseInt(numStr.charAt(i))
-        sum+=n**count
-        i--
-    }
-    console.log(`Number ${num} is ${num != sum ? "not" :""} amstrong number`)
